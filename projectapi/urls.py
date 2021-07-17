4a7daff07250c92b0df8a1d99ac79126adb56654
cheruiyotkelvin <ronohkelvin99@gmail.com>
2021-07-17 13:21:21 +0300

    configure the app urls

--- projectapi/urls.py
+++ projectapi/urls.py
@@ -1,15 +1,12 @@
-
 from django.contrib import admin
-from django.conf.urls import url, include
+from django.urls import path,include
+from django.contrib.auth import views
+from django.conf import settings
+from django.conf.urls.static import static
 from django_registration.backends.one_step.views import RegistrationView
 
 
 urlpatterns = [
-    url('admin/', admin.site.urls),
-    url('', include('api.urls')),
-    url('accounts/register/', RegistrationView.as_view(success_url='/'),name='django_registration_register'),
-    url('accounts/', include('django.contrib.auth.urls')),
-    url('accounts/', include('django_registration.backends.one_step.urls')),
-
-
+    path('admin/', admin.site.urls),
+    path('',include('api.urls')),
 ]
