ade9c1c15a857024eaf831ad70b685d7d9f962fa
cheruiyotkelvin <ronohkelvin99@gmail.com>
2021-07-19 18:51:09 +0300

    display the projct details i the bio

--- api/views.py
+++ api/views.py
@@ -141,4 +141,4 @@ def comment(request, id):
         'form': form,
         'comments':comments,
     }
-    return render(request, 'post.html', params)
+    return render(request, 'project.html', params)
